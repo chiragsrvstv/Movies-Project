16f9eb9a1ea4f74dacc1ade313b3c1abd7b26015
chiragsrvstv <chirag.srivastava@live.com>
2018-06-19 18:26:36 +0530

    Changes after review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,37 +1,54 @@
 import media
-import fresh_tomatoes #This module converts list of given movie instances to web page
+import fresh_tomatoes
+# This module converts list of given movie instances to web page
 
+# Creating instances from class Movie by passing arguments to them
+inception = media.Movie(
+                        "Inception", "Story of dreams and reality",
+                        '''https://www.warnerbros.com/sites/default/files/inception_keyart.jpg''',
+                        "https://www.youtube.com/watch?v=M7O44CUIRYU"
+                        )
 
-#Creating instances from class Movie
+jurassic_world = media.Movie(
+                            "Jurassic World Fallen Kingdom", '''Story of
+                            experimentally created Dinosaurs and their
+                            survival''', '''https://i1.wp.com/bestoftheyear.in/wp-content/uploads/2018/02/Jurassic-World-Fallen-Kingdom.jpg?fit=400%2C634&ssl=1''',
+                            "https://www.youtube.com/watch?v=1FJD7jZqZEk"
+                            )
 
+batman_vs_superman = media.Movie(
+                                "Batman VS Superman",
+                                "Two DC superheroes stand face to face",
+                                '''http://www.movies.ie/wp-content/uploads/2016/03/BVS-MoviesPlus-HD-382x215.-72-pixels.jpg''',
+                                "https://www.youtube.com/watch?v=IwfUnkBfdZ4"
+                                )
 
-inception = media.Movie("Inception", "Story of dreams and reality",
-                        "https://2.bp.blogspot.com/-uZUMCsz_H4M/TWaqInlGSmI/AAAAAAAABbc/2Xbu9kylMrQ/s1600/Inception-Widescreen-Wallpaper-1920x1200-2.jpg" ,
-                        "https://www.youtube.com/watch?v=M7O44CUIRYU")  
+three_hundred = media.Movie(
+                            "300",
+                            "Story of 300 hundred spartans and thier bravery",
+                            '''https://crypticrock.com/wp-content/uploads/2014/01/300-slide.jpg''',
+                            "https://www.youtube.com/watch?v=UrIbxk7idYA"
+                            )
 
-jurassic_world = media.Movie("Jurassic World Fallen Kingdom", "Story of experimentally created Dinosaurs and their survival",
-                     "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapersite.com%2Fimages%2Fwallpapers%2Fjurassic-world-fallen-kingdom-1366x768-hd-2018-8342.jpg&f=1",
-                     "https://www.youtube.com/watch?v=1FJD7jZqZEk")
+twenty_three = media.Movie(
+                            "The Number 23",
+                            '''A psychological thriller film about
+                            obsession and madness''',
+                            '''http://www.cinemagora.co.uk/images/films/12/48412-b-the-number-23.jpg''',
+                            "https://www.youtube.com/watch?v=TUTlOC4mVQ8"
+                           )
 
-batman_vs_superman = media.Movie("Batman VS Superman", "Two DC superheroes stand face to face",
-                               "http://www.designbolts.com/wp-content/uploads/2015/12/Superman-vs-batman_wallpaper_HD.jpg",
-                               "https://www.youtube.com/watch?v=IwfUnkBfdZ4")
+fight_club = media.Movie(
+                        "Fight Club",
+                        '''Story of a man and his miserable life taking
+                        a huge turn''',
+                        '''http://www.flore-maquin.com/wp-content/uploads/Fight_club_RVB_72.jpg''',
+                        "https://www.youtube.com/watch?v=SUXWAEX2jlg"
+                        )
 
-three_hundred = media.Movie("300", "Story of three hundred spartans and thier bravery",
-                            "http://static.rogerebert.com/uploads/movie/movie_poster/300-2006/large_4AmPMxTs1zSdCK0eCacj0kBgOMV.jpg",
-                            "https://www.youtube.com/watch?v=UrIbxk7idYA")
-
-twenty_three = media.Movie("The Number 23", "A psychological thriller film about obsession and madness",
-                           "https://fmgreviews.files.wordpress.com/2014/06/carey-movie-23jpg.jpg",
-                           "https://www.youtube.com/watch?v=TUTlOC4mVQ8")
-
-fight_club = media.Movie("Fight Club", "Story of a man and his miserable life taking a huge turn",
-                         "https://3.bp.blogspot.com/_3N0VetpYvQE/S9jg0AGYwyI/AAAAAAAACz8/OP6C90Or-5s/s1600/Fight_Club_3.jpg",
-                         "https://www.youtube.com/watch?v=SUXWAEX2jlg")
-
-
-movie_list = [three_hundred , inception, batman_vs_superman, twenty_three, fight_club, jurassic_world] 
-
-fresh_tomatoes.open_movies_page(movie_list) #Giving list of instances (movies) to open_movie_page of fresh tomato 
-
-
+movie_list = [
+                three_hundred, inception, batman_vs_superman,
+                twenty_three, fight_club, jurassic_world
+             ]
+fresh_tomatoes.open_movies_page(movie_list)
+# Giving list of instances (movies) to open_movie_page of fresh tomato
--- media.py
+++ media.py
@@ -1,11 +1,20 @@
 class Movie():
-    """Gives info about movies"""           #This is information about the class movie for predifened class variable
-    
-    
-    def __init__(self, title, story, poster_image_url, trailer_youtube_url):  #Defining class movie with constructor
+    """ Gives information about movies.
+
+    Argument:
+    title -- The title of the movie
+    story -- The storyline of the movie
+    poster_image_url -- Movie's poster image url
+    trailer_youtube_url -- Movie's tailer youtube url
+
+    It shows the information like title, storyline,
+    poster image url and trailer's youtube url
+    of the movies."""
+# This is documentation about the class movie
+
+    def __init__(self, title, story, poster_image_url, trailer_youtube_url):
+        # Defining class movie that takes arguments
         self.title = title
         self.story = story
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-    
