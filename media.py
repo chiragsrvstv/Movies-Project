7ef4e132166c9cf682fac394dcfd9075f04d122f
chiragsrvstv <chirag.srivastava@live.com>
2018-06-19 21:08:09 +0530

    final changes after review

--- media.py
+++ media.py
@@ -3,15 +3,14 @@ class Movie():
     """ Gives information about movies.
 
     Argument:
-    title -- The title of the movie
-    story -- The storyline of the movie
-    poster_image_url -- Movie's poster image URL
-    trailer_youtube_url -- Movie's tailer youtube URL
+    title -- The title of the movie (string)
+    story -- The storyline of the movie (string)
+    poster_image_url -- Movie's poster image URL (string)
+    trailer_youtube_url -- Movie's tailer youtube URL (string)
 
-    It shows the information like title, storyline,
+    It shows information like title, storyline,
     poster image url and trailer's youtube URL
-    of the movies."""
-
+    of the movie."""
 
     def __init__(self, title, story, poster_image_url, trailer_youtube_url):
         # Defining class movie that takes arguments
